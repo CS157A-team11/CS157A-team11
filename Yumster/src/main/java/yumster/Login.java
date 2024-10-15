9dc2a81be4cd406be8e2c3f1df3a8414f8ac2039
probablyanasian <43358667+probablyanasian@users.noreply.github.com>
2024-10-14 22:52:41 -0700

    add login.java

---
+++ Yumster/src/main/java/yumster/Login.java
@@ -0,0 +1,68 @@
+package yumster;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+
+/**
+ * Servlet implementation class Register
+ */
+@WebServlet("/api/v1/login")
+public class Login extends HttpServlet {
+	Argon2PasswordEncoder encoder = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+	private static final long serialVersionUID = 1L;
+	
+	// https://emailregex.com/
+    Pattern pattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])", Pattern.CASE_INSENSITIVE);
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("application/json");
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("loginPassword");
+		User user = new User();
+		if (username.indexOf('@') == -1) {
+			// Handle as username
+			User.getByUsername(username, user);
+			if (!encoder.matches(password, user.getPasswordHash())) {
+				Response res = new Response("error", "Your input does not match our records.");
+				response.getWriter().print(res.toJson());
+				return;
+			}
+			
+		} else {
+			// Handle as email
+		}
+	}
+}
